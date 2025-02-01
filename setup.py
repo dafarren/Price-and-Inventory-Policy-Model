bbf3648e224be436f75c817e40fea829325d0322
dafarren <78431495+dafarren@users.noreply.github.com>
2025-02-01 15:56:46 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MiPaqueteEjemplo",      # Nombre del paquete
+    name="Price-and-Inventory-Policy-Functions",      # Nombre del paquete
     version="0.1",
     packages=find_packages(),
     py_modules=['my_functions'],    # Indicamos el módulo que queremos incluir
     install_requires=[
         "prophet"
     ],
-    description="Un paquete de ejemplo con dos funciones: una que multiplica y otra que suma.",
+    description="Un paquete de ejemplo con dos funciones: una que multiplica y otra que suma",
     author="Tu Nombre",
     author_email="tu_email@example.com",
 )
