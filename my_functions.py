238e867603ab737c423bc88038e4e9944d94f248
dafarren <78431495+dafarren@users.noreply.github.com>
2025-02-01 11:53:25 -0300

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,8 @@
+%%writefile my_functions.py
 def mi_funcion(x):
-    """Función de ejemplo que multiplica el valor x por 2."""
+    """Función que multiplica el valor x por 2."""
     return x * 2
 
+def otra_funcion(x):
+    """Función que suma 5 al valor x."""
+    return x + 5
